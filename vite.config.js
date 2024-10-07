c74da1d342a5a986b13bda8831769b43db7b7092
SaidXIX <sbouziani@paidflow.co>
2024-10-07 18:53:29 +0100

    removed .env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from "path";
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,37 +9,37 @@ export default defineConfig({
   resolve: {
     alias: [
       {
-        find: "@assets",
-        replacement: path.resolve(__dirname, "src/assets"),
+        find: '@assets',
+        replacement: path.resolve(__dirname, 'src/assets')
       },
       {
-        find: "@components",
-        replacement: path.resolve(__dirname, "src/components"),
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components')
       },
       {
-        find: "@i18n",
-        replacement: path.resolve(__dirname, "src/i18n"),
+        find: '@i18n',
+        replacement: path.resolve(__dirname, 'src/i18n')
       },
       {
-        find: "@layouts",
-        replacement: path.resolve(__dirname, "src/layouts"),
+        find: '@layouts',
+        replacement: path.resolve(__dirname, 'src/layouts')
       },
       {
-        find: "@routes",
-        replacement: path.resolve(__dirname, "src/routes"),
+        find: '@routes',
+        replacement: path.resolve(__dirname, 'src/routes')
       },
       {
-        find: "@themes",
-        replacement: path.resolve(__dirname, "src/themes"),
+        find: '@themes',
+        replacement: path.resolve(__dirname, 'src/themes')
       },
       {
-        find: "@utils",
-        replacement: path.resolve(__dirname, "src/utils"),
+        find: '@utils',
+        replacement: path.resolve(__dirname, 'src/utils')
       },
       {
-        find: "@views",
-        replacement: path.resolve(__dirname, "src/views"),
-      },
+        find: '@views',
+        replacement: path.resolve(__dirname, 'src/views')
+      }
     ]
   }
 })
